08e11da510ca6f99c717402396770fd107fa80ee
Matthew Todd Hayton <todd.hayton@gmail.com>
2018-06-18 11:38:29 -0400

    Add support for redirection to original URL when shortened URL is visited

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/api/shorturl/new', (req, res) => {
     processRequest(req, res);
+});
+
+app.get('/api/shorturl/:shortened_url([a-zA-Z0-9]+)', (req, res) => {
+    Url.findOne({ shortened: req.params.shortened_url }).then((url) => {
+        if (url === null) {
+            return res.status(404).send({
+                'error': 'URL not found'
+            });            
+        }
+
+        res.redirect(url.original);
+    }).catch((e) => {
+        console.log('Error - ', e);        
+        res.status(400).send({e});
+    });
 });
 
 app.get('/api/shorturl/urls', (req, res) => {
